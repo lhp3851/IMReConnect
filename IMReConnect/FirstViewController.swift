d83a425eca93548cd5a33cd002e2df9b4bd5202f
lhp3851 <lhp3851@163.com>
2016-12-07 10:22:24 +0800

    Initial Commit

---
+++ IMReConnect/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IMReConnect
+//
+//  Created by LiuXuan on 2016/12/7.
+//  Copyright © 2016年 Xuan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
